d7537dc5b2b153d8386f946399ce8fe032d9ddf3
DrProfWallaby <joshuadiamond91@gmail.com>
2026-06-13 13:41:56 -0500

    implemented function to split markdowned text from raw text

---
+++ src/inline_markdown.py
@@ -0,0 +1,23 @@
+from textnode import TextNode, TextType
+
+def split_nodes_delimiter(
+        old_nodes: list[TextNode],
+        delimiter: str,
+        text_type: TextType
+    ) -> list[TextNode]:
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+        split_old_node = old_node.text.split(delimiter)
+        if len(split_old_node) % 2 == 0:
+            raise Exception("Invalid Markdown syntax")
+        for i in range(len(split_old_node)):
+            if not split_old_node[i]:
+                continue
+            if i % 2 == 0:
+                new_nodes.append(TextNode(split_old_node[i], TextType.TEXT))
+            else:
+                new_nodes.append(TextNode(split_old_node[i], text_type))
+    return new_nodes
---
+++ src/test_inline_markdown.py
@@ -0,0 +1,58 @@
+import unittest
+from inline_markdown import split_nodes_delimiter
+from textnode import TextNode, TextType
+
+class TestSplitNodeDelimiter(unittest.TestCase):
+    def test_code_delimiter(self):
+        node = TextNode(
+            "this is a test string with `code snippet` in it",
+            TextType.TEXT
+        )
+        test_case = split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertEqual(test_case[0].text, "this is a test string with ")
+        self.assertEqual(test_case[1].text, "code snippet")
+        self.assertEqual(test_case[2].text, " in it")
+
+    def test_bold_delimiter(self):
+        node = TextNode(
+            "this is a test string with **bold words** in it",
+            TextType.TEXT
+        )
+        test_case = split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertEqual(test_case[0].text, "this is a test string with ")
+        self.assertEqual(test_case[1].text, "bold words")
+        self.assertEqual(test_case[2].text, " in it")
+
+    def test_italic_delimiter(self):
+        node = TextNode(
+            "this is a test string with _italic words_ in it",
+            TextType.TEXT
+        )
+        test_case = split_nodes_delimiter([node], "_", TextType.ITALIC)
+        self.assertEqual(test_case[0].text, "this is a test string with ")
+        self.assertEqual(test_case[1].text, "italic words")
+        self.assertEqual(test_case[2].text, " in it")
+    
+    def test_non_text_node(self):
+        node = TextNode(
+            "_this is a test string with italic words in it_",
+            TextType.ITALIC
+        )
+        test_case = split_nodes_delimiter([node], "_", TextType.ITALIC)
+        self.assertEqual(test_case[0], node)
+
+    def test_invalid_markdown(self):
+        node = TextNode(
+            "this is a test string with **bold words in it",
+            TextType.TEXT
+        )
+        with self.assertRaises(Exception):
+            split_nodes_delimiter([node], "**", TextType.BOLD)
+
+    def test_starts_with_delimiter(self):
+        node = TextNode(
+            "_this is a test string with italic words in it_",
+            TextType.TEXT
+        )
+        test_case = split_nodes_delimiter([node], "_", TextType.ITALIC)
+        self.assertEqual(test_case[0].text, "this is a test string with italic words in it")
