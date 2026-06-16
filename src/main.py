787b14eef7a89e07013a771797e520e775bc878e
DrProfWallaby <joshuadiamond91@gmail.com>
2026-06-16 08:12:53 -0500

    added function to copy static folder over public folder

--- src/main.py
+++ src/main.py
@@ -1,7 +1,23 @@
 import os
+import shutil
 
 def main():
-    pass
+        static = os.path.abspath(f"./static")
+        public = os.path.abspath(f"./public")
+        if os.path.exists(public):
+            shutil.rmtree(public)
+        static_to_public(static, public)
+
+def static_to_public(src: str, dst: str) -> None:
+    if not os.path.exists(dst):
+        os.mkdir(dst)
+    for item in os.listdir(src):
+        if os.path.isdir(os.path.join(src, item)):
+            static_to_public(os.path.join(src, item), os.path.join(dst, item))
+        else:
+            shutil.copy(os.path.join(src, item), os.path.join(dst, item))
+
+
 
 if __name__ == "__main__":
     main()
